b2f76b0d558c8ae7bba9b6111231a626946e4f07
Wangkan <rockwk@msn.com>
2018-02-01 22:30:58 +0800

    add state

---
+++ StateDemo/StateDemo/context.swift
@@ -0,0 +1,42 @@
+//
+//  context.swift
+//  Mechanic - State
+//
+//  Created by Reza Shirazian on 2016-04-19.
+//  Copyright © 2016 Reza Shirazian. All rights reserved.
+//
+
+import Foundation
+
+class Context {
+  private var state: State = SubmittedState()
+
+  func getMessageToCustomer() -> String {
+    return state.getMessageToCustomer(context: self)
+  }
+
+  func getPrice() -> Double? {
+    return state.getPrice(context: self)
+  }
+
+  func getReceipt() -> Receipt? {
+    return state.getReceipt(context: self)
+  }
+
+    func changeStateToReady(price: Double) {
+    state = ReadyState(price: price)
+  }
+
+  func changeStateToPending() {
+    state = PendingState()
+  }
+
+  func changeStateToBooked(price: Double, mechanic: Mechanic) {
+    state = BookedState(price: price, mechanic: mechanic)
+  }
+
+  func changeStateToCompleted(price: Double, mechanic: Mechanic, receipt: Receipt) {
+    state = CompletedState(price: price, mechanic: mechanic, receipt: receipt)
+  }
+
+}
---
+++ StateDemo/StateDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StateDemo
+//
+//  Created by 王侃 on 2018/2/1.
+//  Copyright © 2018年 王侃. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
